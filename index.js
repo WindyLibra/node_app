16ce947ccc87a637407d062bb7017dfc722b039a
David Makaryan <windylibra3000@gmail.com>
2025-01-15 22:10:05 +0400

    omg did it

--- index.js
+++ index.js
@@ -1,13 +1,49 @@
 const express = require('express');
-
+const { request } = require('http');
+const PORT = 8080;
 const app = express();
+app.use(express.json());
+
+app.listen(PORT, () => console.log(`app available on http://localhost:${PORT}`))
 
 const { readFile } = require('fs').promises;
 
-app.get('/', async (request, response) => {
+/*
+app.get('/', async (req, res) => {
 
-    response.send(await readFile('./home.html', 'utf8'));
+    res.send(await readFile('./home.html', 'utf8'));
+
+});
+*/
+
+app.get('/game', async (req, res) => {
+
+    res.status(200).send({
+        name: 'Human with an Umbrella',
+        developer: 'Sunfire Studios',
+    });
 
 });
 
-app.listen(process.env.PORT || 3000, () => console.log('app available on http://localhost:3000'))
+app.post('/game/:id', async (req, res) => {
+
+    const { id } = req.params;
+    const { name } = req.body;
+    const { developer } = req.body;
+
+    if (!name) {
+        res.status(400).send("We need a name")
+    }
+
+    if (!developer) {
+        res.status(400).send("We need the developer")
+    }
+
+    res.send({
+        name: `${name}`,
+        developer: `${developer}`,
+        id: `and the id: ${id}`,
+    })
+    
+});
+
